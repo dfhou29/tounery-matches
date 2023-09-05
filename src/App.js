9d16fa2445bec7ee770cbc534dfac1660ef282df
Difan <dfhou29@gmail.com>
2023-09-05 01:04:43 -0700

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import PlayerList from "./components/PlayerList";
 import MatchList from "./components/MatchList";
+import matchData from "./data/matchData";
+import playerData from "./data/playerData";
+import {preparePlayerData, addWinsToPlayers} from "./helpers/playerHelpers";
 
 function App() {
+
+  const playerDataArr = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArr, matchData);
+  console.log(parsedPlayerData);
+
   return (
     <div className="App">
       <h1>
         Tourney Matches <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList/>
-      <MatchList/>
+      <PlayerList playerData={parsedPlayerData}/>
+      <MatchList matchData={matchData} />
     </div>
   );
 }
