0ff401a51fce6b7aba82be1a40358c94bccdc8f4
Difan <dfhou29@gmail.com>
2023-09-04 23:02:05 -0700

    initialize components

---
+++ src/components/MatchList.jsx
@@ -0,0 +1,9 @@
+function MatchList() {
+
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {/* Matches will be shown here */}
+    </section>
+  )
+}
---
+++ src/components/PlayerList.jsx
@@ -0,0 +1,11 @@
+function PlayerList() {
+
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {/* Players will be shown here */}
+    </section>
+  )
+}
+
+export default PlayerList;
