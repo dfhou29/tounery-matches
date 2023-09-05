1e9745690a4e695279f93e1fd3f7d3d8c927acb1
Difan <dfhou29@gmail.com>
2023-09-05 01:03:53 -0700

    fix minor bug

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,17 +1,20 @@
-export const preparePlayerData  = (playerData) => {
+export const preparePlayerData = (playerData) => {
   return Object.values(playerData);
 }
 
 export const addWinsToPlayers = (playerDataArr, matchData) => {
-  for (const player of playerDataArr) {
+  const parsedPlayerData = [...playerDataArr];
+  for (const player of parsedPlayerData) {
 
     player.wins = matchData.reduce((acc, cur) => {
-        if (cur.winner === player.gamerTag) {
-          return acc++;
-        } else {
-          return acc;
-        }
+      if (cur.winner === player.gamerTag) {
+        return acc + 1;
+      } else {
+        return acc;
+      }
     }, 0);
 
   }
+
+  return parsedPlayerData;
 }
